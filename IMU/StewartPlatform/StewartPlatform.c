9acdfd16d21e136029debad28c80c68c38d33a80
Ademola Adedeji <a.mola.dev@gmail.com>
2024-02-26 12:42:33 -0400

    ✨ feat(StewartPlatform): rewrite using Robert Eisele's implementation
    https://github.com/rawify/Stewart.js/tree/master

--- IMU/StewartPlatform/StewartPlatform.c
+++ IMU/StewartPlatform/StewartPlatform.c
@@ -1,200 +1,184 @@
 #include <math.h>
 #include <stdint.h>
 
-#include "StewartPlatform.h"
+#define LEGS_COUNT 6
 
-#ifndef M_PI
-#define M_PI 3.14159265358979323846264338327950288f
-#endif
+#define sqr(num)                 (num * num)
+#define InRange(angle, min, max) (angle < min ? min : angle > max ? max : angle)
 
-#define HALF_PI        M_PI / 2
-#define ONE_THIRD_PI   M_PI / 3
-#define TWO_THIRDS_PI  2 * ONE_THIRD_PI
-#define FOUR_THIRDS_PI 4 * ONE_THIRD_PI
+#define BASE_OUTER_RADIUS     0
+#define BASE_INNER_RADIUS     0
+#define PLATFORM_OUTER_RADIUS 0
+#define PLATFORM_INNER_RADIUS 0
 
-static float beta[ARMS_COUNT] = {0};
-static float home_pos[AXES_COUNT] = {0};
-static float B[AXES_COUNT][ARMS_COUNT] = {0}; // Coordinate of the points where servo arms are attached to the corresponding servo axis.
-static float P[AXES_COUNT][ARMS_COUNT] = {0}; // Coordinates of the points where the rods are attached to the platform.
+#define SERVO_ANGLE_MIN -90
+#define SERVO_ANGLE_MAX 90
 
-static float lhl = 0;
-static float ldl = 0;
+typedef struct Coords {
+  float x;
+  float y;
+  float z;
+} Coords;
 
-static float lll[ARMS_COUNT] = {0};
-static float l[AXES_COUNT][ARMS_COUNT] = {0};
+typedef struct Legs {
+  float  servoAngle;
+  float  sinBeta;       // Sin of Pan angle of motors in base plate
+  float  cosBeta;       // Cos of Pan angle of motors in base plate
+  Coords baseJoint;     // base joints in base frame
+  Coords platformJoint; // platform joints in platform frame
+} Legs;
 
-static void StewartPlatform_RotX(float roll, float rotXMatrix[AXES_COUNT][AXES_COUNT]);
-static void StewartPlatform_RotY(float pitch, float rotYMatrix[AXES_COUNT][AXES_COUNT]);
-static void StewartPlatform_RotZ(float yaw, float rotZMatrix[AXES_COUNT][AXES_COUNT]);
+typedef struct Quaternion {
+  float w;
+  float x;
+  float y;
+  float z;
+} Quaternion;
 
-/**
- * @brief
- * Yeok 2022
- * Stewart Platform Python Implementation
- * Uses 6 Rotational Servos
+static float rodLength = 0;
+static float hornLength = 0;
 
- * @param r_B Radius for circumscribed circle where all the anchor points for servo shaft lie on (Bottom)
- * @param r_P Radius for circumscribed circle where all anchor points for platform lie on (Top)
- * @param __lhl length of servo horn
- * @param __ldl length of rod
- * @param gamma_B Half of angle between two anchors on the base
- * @param gamma_P Half of angle between two anchors on the platform
- * @param ref_rotation rotates the entire platform in the z-axis to align with your applications
- */
-void StewartPlatform_Init(float r_B, float r_P, float __lhl, float __ldl, float gamma_B, float gamma_P, float ref_rotation) {
-  uint8_t armIdx;
-  uint8_t axisIdx;
-  float   z;
+static Coords T0 = {0};
+static Legs   legs[] = {0};
 
-  // Psi_B (Polar coordinates)
-  float_t psi_B[ARMS_COUNT] = {
-      -gamma_B,                 // Arm #1
-      gamma_B,                  // Arm #2
-      TWO_THIRDS_PI - gamma_B,  // Arm #3
-      TWO_THIRDS_PI + gamma_B,  // Arm #4
-      FOUR_THIRDS_PI - gamma_B, // Arm #5
-      FOUR_THIRDS_PI + gamma_B  // Arm #6
-  };
-  // psi_P (Polar coordinates)
-  // Direction of the points where the rod is attached to the platform.
-  float_t psi_P[ARMS_COUNT] = {
-      ONE_THIRD_PI + FOUR_THIRDS_PI + gamma_P, // Arm #1
-      ONE_THIRD_PI - gamma_P,                  // Arm #2
-      ONE_THIRD_PI + gamma_P,                  // Arm #3
-      ONE_THIRD_PI + TWO_THIRDS_PI - gamma_P,  // Arm #4
-      ONE_THIRD_PI + TWO_THIRDS_PI + gamma_P,  // Arm #5
-      ONE_THIRD_PI + FOUR_THIRDS_PI - gamma_P, // Arm #6
-  };
+static Coords l[] = {0}; // vector from Base anchor to Platform anchor
 
-  beta[0] = HALF_PI + M_PI;                  // Arm #1
-  beta[1] = HALF_PI;                         // Arm #2
-  beta[2] = TWO_THIRDS_PI + HALF_PI + M_PI;  // Arm #3
-  beta[3] = TWO_THIRDS_PI + HALF_PI;         // Arm #4
-  beta[4] = FOUR_THIRDS_PI + HALF_PI + M_PI; // Arm #5
-  beta[5] = FOUR_THIRDS_PI + HALF_PI;        // Arm #6
+void StewartPlatform_RotateVector(Coords dest, Quaternion orientation, Coords vector);
+void StewartPlatform_GetLegs(Legs    legs[],
+                             uint8_t hornDirection, // If horns are pointed outwards 0, otherwise 1
+                             float   shaftDistance  // Distance between servo pair
+);
 
-  for (armIdx = 0; armIdx < ARMS_COUNT; armIdx++) {
-    beta[armIdx] += ref_rotation;
-    psi_B[armIdx] += ref_rotation;
-    psi_P[armIdx] += ref_rotation;
-  }
+void StewartPlatform_Init(float _rodLength, float _hornLength, float shaftDistance) {
+  uint8_t legIdx = 0;
+  Legs    legs[LEGS_COUNT] = {0};
 
-  for (axisIdx = 0; axisIdx < AXES_COUNT; axisIdx++) {
-    for (armIdx = 0; armIdx < ARMS_COUNT; armIdx++) {
-      if (axisIdx == 2) {
-        // Leave Z-axis values at 0
-        B[axisIdx][armIdx] = 0;
-        P[axisIdx][armIdx] = 0;
-      } else {
-        // Transposed array with axis as rows and arms as column
-        B[axisIdx][armIdx] = r_B * psi_B[armIdx];
-        P[axisIdx][armIdx] = r_P * psi_P[armIdx];
-      }
-    }
-  }
+  StewartPlatform_GetLegs(legs, 0, shaftDistance);
 
-  // Definition of the platform home position.
-  z = sqrtf(powf(__ldl, 2) + powf(__lhl, 2) - powf(P[0] - B[0], 2) - powf(P[1] - B[1], 2));
-  home_pos[0] = 0;
-  home_pos[1] = 0;
-  home_pos[2] = z;
+  rodLength = _rodLength;
+  hornLength = _hornLength;
 
-  lhl = __lhl;
-  ldl = __ldl;
+  T0.z = sqrtf(                                            //
+      sqr(rodLength) +                                     // sqr(∣d∣)
+      sqr(hornLength) -                                    // sqr(∣h∣)
+      sqr(legs[0].platformJoint.x - legs[0].baseJoint.x) - // sqr(pk(x) - bk(x))
+      sqr(legs[0].platformJoint.y - legs[0].baseJoint.y)   // sqr(pk(y) - bk(y))
+  );
 }
 
-static void StewartPlatform_RotX(float roll, float rotXMatrix[AXES_COUNT][AXES_COUNT]) {
-  rotXMatrix[0][0] = 1;
-  rotXMatrix[0][1] = 0;
-  rotXMatrix[0][2] = 0;
+void getHexPlate(Coords ret[], float r_i, float r_o, float rot) {
+  uint8_t legIdx = 0;
 
-  rotXMatrix[1][0] = 0;
-  rotXMatrix[1][1] = cosf(roll);
-  rotXMatrix[1][2] = sinf(roll);
+  float ap, phi;
+  float a_2 = (2 * r_i - r_o) / sqrtf(3);
 
-  rotXMatrix[2][0] = 0;
-  rotXMatrix[2][1] = -sinf(roll);
-  rotXMatrix[2][2] = cosf(roll);
+  for (legIdx = 0; legIdx < 6; legIdx++) {
+    phi = ((legIdx - (legIdx % 2)) / 3) * M_PI + rot;
+    ap = a_2 * powf(-1, legIdx);
+
+    ret[legIdx].x = r_o * cosf(phi) + ap * sinf(phi);
+    ret[legIdx].y = r_o * sinf(phi) - ap * cosf(phi);
+  }
 }
 
-static void StewartPlatform_RotY(float pitch, float rotYMatrix[AXES_COUNT][AXES_COUNT]) {
-  rotYMatrix[0][0] = cosf(pitch);
-  rotYMatrix[0][1] = 0;
-  rotYMatrix[0][2] = -sinf(pitch);
+void StewartPlatform_GetLegs(Legs legs[], uint8_t hornDirection, float shaftDistance) {
+  uint8_t armIdx = 0;
 
-  rotYMatrix[1][0] = 0;
-  rotYMatrix[1][1] = 1;
-  rotYMatrix[1][2] = 0;
+  Coords baseInts[] = {0};
+  Coords platformInts[] = {0};
 
-  rotYMatrix[2][0] = sinf(pitch);
-  rotYMatrix[2][1] = 0;
-  rotYMatrix[2][2] = cosf(pitch);
+  float baseCx, baseCy, baseNx, baseNY;
+  float platCx, platCy, platNx, platNY;
+  float baseMidX, baseMidY;
+  float platMidX, platMidY;
+  float lenBaseSide, pm;
+  float baseDX, baseDY;
+  float motorRotation;
+
+  getHexPlate(baseInts, BASE_OUTER_RADIUS, BASE_INNER_RADIUS, 0);
+  getHexPlate(platformInts, PLATFORM_OUTER_RADIUS, PLATFORM_INNER_RADIUS, 0);
+
+  for (armIdx = 0; armIdx < 6; armIdx++) {
+    uint8_t midK = armIdx | 1;
+
+    baseCx = baseInts[midK].x;
+    baseCy = baseInts[midK].y;
+    baseNx = baseInts[(midK + 1) % 6].x;
+    baseNY = baseInts[(midK + 1) % 6].y;
+
+    platCx = platformInts[midK].x;
+    platCy = platformInts[midK].y;
+    platNx = platformInts[(midK + 1) % 6].x;
+    platNY = platformInts[(midK + 1) % 6].y;
+
+    baseDX = baseNx - baseCx;
+    baseDY = baseNY - baseCy;
+    lenBaseSide = hypotf(baseDX, baseDY);
+
+    pm = powf(-1, armIdx);
+
+    baseMidX = (baseCx + baseNx) / 2;
+    baseMidY = (baseCy + baseNY) / 2;
+
+    platMidX = (platCx + platNx) / 2;
+    platMidY = (platCy + platNY) / 2;
+
+    baseDX /= lenBaseSide;
+    baseDY /= lenBaseSide;
+
+    motorRotation = atan2f(baseDY, baseDX) + ((armIdx + hornDirection) % 2) * M_PI;
+
+    legs[armIdx].baseJoint.x = baseMidX + baseDX * shaftDistance * pm;
+    legs[armIdx].baseJoint.y = baseMidY + baseDY * shaftDistance * pm;
+    legs[armIdx].baseJoint.z = 0;
+
+    legs[armIdx].platformJoint.x = platMidX + baseDX * shaftDistance * pm;
+    legs[armIdx].platformJoint.y = platMidY + baseDY * shaftDistance * pm;
+    legs[armIdx].platformJoint.z = 0;
+
+    legs[armIdx].sinBeta = sinf(motorRotation);
+    legs[armIdx].cosBeta = cosf(motorRotation);
+  }
 }
 
-static void StewartPlatform_RotZ(float yaw, float rotZMatrix[AXES_COUNT][AXES_COUNT]) {
-  rotZMatrix[0][0] = cosf(yaw);
-  rotZMatrix[0][1] = sinf(yaw);
-  rotZMatrix[0][2] = 0;
+void StewartPlatform_Update(Coords translation, Quaternion orientation) {
+  Coords  o = {0};
+  uint8_t legIdx = 0;
 
-  rotZMatrix[1][0] = -sinf(yaw);
-  rotZMatrix[1][1] = cosf(yaw);
-  rotZMatrix[1][2] = 0;
+  float gk, ek, fk;
 
-  rotZMatrix[2][0] = 0;
-  rotZMatrix[2][1] = 0;
-  rotZMatrix[2][2] = 1;
+  for (legIdx = 0; legIdx < LEGS_COUNT; legIdx++) {
+    StewartPlatform_RotateVector(o, orientation, legs[legIdx].platformJoint);
+
+    l[legIdx].x = translation.x + o.x - legs[legIdx].baseJoint.x;
+    l[legIdx].y = translation.y + o.y - legs[legIdx].baseJoint.y;
+    l[legIdx].z = translation.z + o.z - legs[legIdx].baseJoint.z + T0.z;
+
+    gk = sqr(l[legIdx].x) + sqr(l[legIdx].y) + sqr(l[legIdx].z) - sqr(rodLength) + sqr(hornLength);
+    ek = 2 * hornLength * l[legIdx].z;
+    fk = 2 * hornLength * ((legs[legIdx].cosBeta * l[legIdx].x) + (legs[legIdx].sinBeta * l[legIdx].y));
+
+    legs[legIdx].servoAngle = InRange(asinf(gk / sqrtf(sqr(ek) + sqr(fk))) - atan2f(fk, ek), SERVO_ANGLE_MIN, SERVO_ANGLE_MAX);
+  }
 }
 
-void StewartPlatform_Calculate(float translate[AXES_COUNT], float rotation[AXES_COUNT], float angles[ARMS_COUNT]) {
-  uint8_t armIdx;
-  uint8_t axisIdx;
-  uint8_t newArmIdx;
-  float   rotMatrixXP = 0;
-  float   R[AXES_COUNT][AXES_COUNT] = {0};
+void StewartPlatform_RotateVector(Coords dest, Quaternion orientation, Coords vector) {
+  float qw = orientation.w;
+  float qx = orientation.x;
+  float qy = orientation.y;
+  float qz = orientation.z;
 
-  float g = 0;
-  float e = 0;
-  float fk = 0;
+  float vx = vector.x;
+  float vy = vector.y;
+  float vz = vector.z;
 
-  float_t rotMatrix[AXES_COUNT][AXES_COUNT] = {
-      {cosf(rotation[1]),                     0,                  -sinf(rotation[1])                   },
-      {sinf(rotation[0]) * sinf(rotation[1]), cosf(rotation[0]),  sinf(rotation[0]) * cosf(rotation[1])},
-      {cosf(rotation[0]) * sinf(rotation[1]), -sinf(rotation[0]), cosf(rotation[0]) * cosf(rotation[1])},
-  };
+  // t = 2q x v
+  float tx = 2 * (qy * vz - qz * vy);
+  float ty = 2 * (qz * vx - qx * vz);
+  float tz = 2 * (qx * vy - qy * vx);
 
-  // s.l =  np.repeat(trans[:, np.newaxis], 6, axis=1) + np.repeat(s.home_pos[:, np.newaxis], 6, axis=1) + np.matmul(R, s.P) - s.B
-  // Calculate length for each leg
-  for (axisIdx = 0; axisIdx < AXES_COUNT; axisIdx++) {
-    for (armIdx = 0; armIdx < ARMS_COUNT; armIdx++) {
-      rotMatrixXP = 0;
-      for (newArmIdx = 0; newArmIdx < AXES_COUNT; newArmIdx++) {
-        rotMatrixXP += rotMatrix[axisIdx][newArmIdx] * P[newArmIdx][armIdx];
-      }
-
-      l[axisIdx][armIdx] = translate[axisIdx] + home_pos[axisIdx] + rotMatrixXP - B[axisIdx][armIdx];
-    }
-  }
-
-  // s.lll = np.linalg.norm(s.l, axis=0)
-  // Calculate the norms for each arm (square root of the sum of each squared coord)
-  for (armIdx = 0; armIdx < ARMS_COUNT; armIdx++) {
-    lll[armIdx] = 0;
-
-    for (axisIdx = 0; axisIdx < AXES_COUNT; axisIdx++) {
-      lll[armIdx] += powf(l[axisIdx][armIdx], 2);
-    }
-
-    lll[armIdx] = sqrtf(lll[armIdx]);
-  }
-
-  // Calculate servo angles for each leg
-  for (armIdx = 0; armIdx < ARMS_COUNT; armIdx++) {
-    // Calculate auxiliary quantities g, f and e
-    g = powf(lll[armIdx], 2) - (powf(ldl, 2) - powf(lhl, 2));
-    e = 2 * lhl * l[2][armIdx];
-    fk = 2 * lhl * ((cosf(beta[armIdx]) * l[0][armIdx]) + (sinf(beta[armIdx]) * l[1][armIdx]));
-
-    // The wanted position could be achieved if the solution of this equation is real for all i
-    angles[armIdx] = asinf(g / sqrtf(powf(e, 2) + powf(fk, 2))) - atan2f(fk, e);
-  }
+  // v + w t + q x t
+  dest.x = vx + qw * tx + qy * tz - qz * ty;
+  dest.y = vy + qw * ty + qz * tx - qx * tz;
+  dest.z = vz + qw * tz + qx * ty - qy * tx;
 }
