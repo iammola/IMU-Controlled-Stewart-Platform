fbf4d9607fb6d50bdf70c3919d676f5a34f852e5
Ademola Adedeji <a.mola.dev@gmail.com>
2024-03-24 10:13:21 -0300

    🚧 wip(StewartPlatform): move with controller/IMU

--- Examples/StewartPlatform/Main.c
+++ Examples/StewartPlatform/Main.c
@@ -15,7 +15,7 @@
 //              < 1=>  1: Joystick
 //          <i> Input control method
 //
-#define CONTROL_METHOD 0
+#define CONTROL_METHOD 1
 
 //-------- <<< end of configuration section >>> ------------------------------
 #include <stdint.h>
@@ -24,17 +24,12 @@
 #include "FPU/fpu.h"
 #include "PLL/PLL.h"
 
-#include "CLI/CLI.h"
-
 #include "IMU/IMU.h"
 #include "Joystick/Joystick.h"
 #include "Pololu Maestro/Maestro.h"
-#include "Quaternion/Quaternion.h"
 #include "StewartPlatform/StewartPlatform.h"
 
 #define SYS_CLOCK 80e6
-
-static char text[CLI_TXT_BUF] = "";
 
 void WaitForInterrupt(void);
 void EnableInterrupts(void);
@@ -47,8 +42,6 @@ int main(void) {
   PLL_Init();              // Initialize the PLL
   FPULazyStackingEnable(); // Enable Floating Point
 
-  CLI_Init(SYS_CLOCK, 115200, WORD_8_BIT, RX_FIFO_OFF, NO_PARITY, ONE_STOP_BIT); // Init UART COM
-
 #if CONTROL_METHOD == 0
   IMU_Init(SYS_CLOCK, &position); // Initialize IMU
   IMU_Enable();
@@ -59,21 +52,26 @@ int main(void) {
   Maestro_Init(SYS_CLOCK); // Initialize Maestro Controller
   StewartPlatform_Init();  // Initialize stewart platform
 
+  for (legIdx = 0; legIdx < Maestro_Channels; legIdx++) { // Set Initial angles
+    Maestro_SetAngle(legIdx, legs[legIdx].servoAngle);
+  }
+
   while (1) {
     WaitForInterrupt();
 
     if (!position.isNew)
       continue;
 
+    DisableInterrupts();
     StewartPlatform_Update(position.translation, position.quaternion);
 
-    DisableInterrupts();
     for (legIdx = 0; legIdx < Maestro_Channels; legIdx++) {
       Maestro_SetAngle(legIdx, legs[legIdx].servoAngle);
     }
-    Maestro_WaitForIdle();
-    EnableInterrupts();
 
     position.isNew = false;
+    EnableInterrupts();
+
+    Maestro_WaitForIdle();
   }
 }
